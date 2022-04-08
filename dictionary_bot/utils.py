bbf83d8d91ec91958ff6dcd3d2e6d8c564b5f39e
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2022-04-08 21:44:16 +0300

    added changes with buttons

--- dictionary_bot/utils.py
+++ dictionary_bot/utils.py
@@ -5,34 +5,32 @@ from dictionary_bot.bot import Commands_of_words
 
 def get_main_word_keyboard(change_lang_code):
     return InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton(
-                    '📝 Добавить слово и перевод в словарь',
-                    callback_data=f'{Commands_of_words.add_word.value}'
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    '🧐 Добавить/Посмотреть контекст',
-                    callback_data=f'{Commands_of_words.examples.value}'
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    '👀 Посмотреть другие переводы',
-                    callback_data=f'{Commands_of_words.change_translation.value}:{change_lang_code}'
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    '🌎 Изменить язык перевода',
-                    callback_data=f'{Commands_of_words.change_lang.value}'
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    '❌ Удалить перевод и слово',
-                    callback_data=f'{Commands_of_words.remove_translation.value}'
-                )
-            ]
-        ])
+        [
+            InlineKeyboardButton(
+                '📝 Добавить слово и перевод в словарь',
+                callback_data=f'{Commands_of_words.add_word.value}'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                '👀 Другие значения',
+                callback_data=f'{Commands_of_words.change_translation.value}:{change_lang_code}'
+            ),
+            InlineKeyboardButton(
+                '🧐 Контекст',
+                callback_data=f'{Commands_of_words.examples.value}'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                '🌎 Изменить язык перевода',
+                callback_data=f'{Commands_of_words.change_lang.value}'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                '❌ Удалить из истории',
+                callback_data=f'{Commands_of_words.remove_translation.value}'
+            )
+        ]
+    ])
